76f4133b42a7fb04c4916bfd22d3a028f6d5d353
HASH512512 <69261186+HASH512512@users.noreply.github.com>
2026-09-11 13:25:38 +0800

    Update diy-part1.sh
    add ua3f

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
+git clone https://github.com/SunBK201/UA3F.git package/UA3F
